b188a0c63b90772f7c5f05597bf6ee4fea514ef4
Jeremy <33503380+jeremy-lockwood@users.noreply.github.com>
2024-04-11 22:11:21 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "My NixOS Flake"; 
 
   inputs = {
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
-      #url = "github:nix-community/home-manager/release-23.11";
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
+      #url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
