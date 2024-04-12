3858f958ee1974e614e705469e200bf40c9e75d0
Jeremy <33503380+jeremy-lockwood@users.noreply.github.com>
2024-04-11 22:08:56 -0400

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "My NixOS Flake"; 
+
+  inputs = {
+    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      #url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations.my-nixos-laptop = nixpkgs.lib.nixosSystem {        
+      system = "x86_64-linux";
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.jeremy = import ./jeremy.nix;
+        }
+      ];
+    };
+  };
+}
